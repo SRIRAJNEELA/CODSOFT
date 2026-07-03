5e8df2c278867f9283c348163291212d759ab0b6
sriraj <neelaomsriraj@gmail.com>
2026-07-04 01:05:43 +0530

    Completed CodSoft Task 1 Portfolio

---
+++ Portfolio/script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    var typed = new Typed('#element', {
+        strings: [
+            'Full Stack Developer',
+            'Web Developer'
+        ],
+        typeSpeed: 60,
+        backSpeed: 50,
+        backDelay: 500,
+        loop: true
+    });
+
+});
